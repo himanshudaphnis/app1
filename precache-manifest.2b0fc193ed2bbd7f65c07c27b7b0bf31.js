3f946039b17ea3480a4a9834daa16426148ae6cc
himanshu khare <khareh57@gmail.com>
2018-12-17 04:38:54 +0530

    Updates

---
+++ precache-manifest.2b0fc193ed2bbd7f65c07c27b7b0bf31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "192e521457dee7d9dc54",
+    "url": "/app1/static/js/runtime~main.192e5214.js"
+  },
+  {
+    "revision": "f8ffb0012ac14d47b88d",
+    "url": "/app1/static/js/main.f8ffb001.chunk.js"
+  },
+  {
+    "revision": "6eb96b7c95ea4493efdb",
+    "url": "/app1/static/js/1.6eb96b7c.chunk.js"
+  },
+  {
+    "revision": "f8ffb0012ac14d47b88d",
+    "url": "/app1/static/css/main.b1580f2e.chunk.css"
+  },
+  {
+    "revision": "3e93945c06df25335b17fc3102804407",
+    "url": "/app1/index.html"
+  }
+];
